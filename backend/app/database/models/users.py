a8ee18a91d063a11a536cb9aa156c795b4f80eae
りおちん <118029.ichikama@gmail.com>
2025-03-17 23:12:37 +0900

    Change: use JST instead of UTC in user model

--- backend/app/database/models/users.py
+++ backend/app/database/models/users.py
@@ -1,15 +1,18 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.orm import relationship
-from datetime import datetime
+from datetime import datetime,timedelta,timezone
 from ..database import Base
 
 class User(Base):
+    # 日本標準時 (JST) を設定
+    JST = timezone(timedelta(hours=9))
+
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     firebase_uid = Column(String, unique=True, nullable=False)
     name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.now(JST))
 
     visits = relationship("Visit", back_populates="user")
