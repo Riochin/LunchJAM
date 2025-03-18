8fcb7a14ffcd80f386c26372e7ac88a244db538a
ayame <ayame2117@gmail.com>
2025-03-18 17:41:34 +0900

    FooterとHeaderの変更　main画面も変更　

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,21 +1,19 @@
-import QRPage from './qr/page';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
-import Congestion from './congestion/page';
+import QRPage from './qr/page';
 
 export default function Home() {
   return (
     <div>
       <header>
-        <Header></Header>
+        <Header />
       </header>
       <main>
-        <QRPage></QRPage>
-        <Congestion></Congestion>
+        <QRPage />
       </main>
 
       <div className="footer">
-        <Footer></Footer>
+        <Footer />
       </div>
     </div>
   );
