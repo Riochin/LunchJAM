c16c41e6e29ef08e19e19c3e9918b09c76cc6397
ayame <ayame2117@gmail.com>
2025-03-18 15:31:50 +0900

    混雑状況表示する画面に現在時刻を表示

--- frontend/src/app/congestion/page.tsx
+++ frontend/src/app/congestion/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import DateTime from '../../../components/DateTime';
 
 const Congestion: React.FC = () => {
   return (
@@ -6,7 +7,9 @@ const Congestion: React.FC = () => {
       <div className="text">現在の状況</div>
       <div className="congestion-icon-left"></div>
       <div className="congestion-text-right">大変混雑している</div>
-      <div className="current-time">◯◯時</div>
+      <div className="current-time">
+        <DateTime></DateTime>
+      </div>
       <div className="graph-container">
         <div className="graph"></div>
       </div>
