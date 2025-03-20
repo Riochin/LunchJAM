f5cd349d44e332fb63bac69dd23469a07bf9330f
ayame <ayame2117@gmail.com>
2025-03-20 23:19:09 +0900

    混雑グラフ表示画面で現在時刻表示するようにした。isita.

--- frontend/src/app/congestion/page.tsx
+++ frontend/src/app/congestion/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import DateTime from '../../../components/DateTime';
 import Header from '../../../components/Header';
+import DateTime from '../../../components/DateTime';
 import Footer from '../../../components/Footer';
 
 const Congestion: React.FC = () => {
